8adf6d8b60c8744a66b7f753ca61abd94e0c0eaf
trieu1212 <lequoctrieu618@gmail.com>
2024-05-20 10:03:56 +0700

    add reader type tag

--- src/components/private/ReaderModal.jsx
+++ src/components/private/ReaderModal.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 import { createReader, getAllReaderType } from '../../apis/Reader'
-import {formatBirthDay} from '../../ultils/helpers'
+import { formatBirthDay } from '../../ultils/helpers'
 const ReaderModal = (props) => {
-    const { setShowModal } = props
+    const { setShowModal, option } = props
     const [readerType, setReaderType] = useState([])
     const [readerName, setReaderName] = useState('')
     const [email, setEmail] = useState('')
@@ -12,7 +12,7 @@ const ReaderModal = (props) => {
     const [readerTypeId, setReaderTypeId] = useState(1)
     const [address, setAddress] = useState('')
     const handleAddReader = async () => {
-        if (email==='' || readerName === '' || birthday === '' || password === '' || readerTypeId === '' || address === '') {
+        if (email === '' || readerName === '' || birthday === '' || password === '' || readerTypeId === '' || address === '') {
             toast.error('Vui lòng nhập đầy đủ thông tin')
         } else {
             const data = {
@@ -32,66 +32,92 @@ const ReaderModal = (props) => {
             }
         }
     }
-    const getReaderType = async()=>{
+    const getReaderType = async () => {
         const response = await getAllReaderType()
         setReaderType(response.data)
     }
-    useEffect(()=>{
+    useEffect(() => {
         getReaderType()
-    },[])
+    }, [])
     return (
         <div>
-            <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50'></div>
-            <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white z-50 p-12'>
-                <h1 className='font-semibold text-[30px]'>Thêm độc giả</h1>
-                <div className='w-[300px]'>
-                    <label className='font-semibold'>Tên độc giả</label>
-                    <input
-                        type="text"
-                        className='w-full p-2 border border-gray-300 rounded-md'
-                        value={readerName}
-                        onChange={(e) => setReaderName(e.target.value)}
-                    />
-                    <label className='font-semibold'>Ngày sinh</label><br />
-                    <input className='border p-1 rounded-md' type="date" name="" id="" 
-                        value={birthday}
-                        onChange={(e) => setBirthday(e.target.value)}
-                    /><br />
-                    <label className='font-semibold' htmlFor="">Password</label>
-                    <input 
-                        type="text" 
-                        className='w-full p-2 border border-gray-300 rounded-md'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <label className='font-semibold'>Email</label>
-                    <input
-                        type="text"
-                        className='w-full p-2 border border-gray-300 rounded-md'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <label className='font-semibold'>Loại độc giả</label>
-                    <select name="" id="" className='px-3 py-1' onChange={(e)=>setReaderTypeId(e.target.value)}>
-                        {readerType.length>0 && readerType.map((item)=>{
-                            return (
-                                <option value={item.reader_type_id}>{item.reader_type}</option>
-                            )
-                        })}
-                    </select><br />
-                    <label className='font-semibold' htmlFor="">Địa chỉ</label>
-                    <input 
-                        type="text"
-                        className='w-full p-2 border border-gray-300 rounded-md'
-                        value={address}
-                        onChange={(e) => setAddress(e.target.value)} 
-                    />
-                </div>
-                <div className='flex justify-end gap-4 mt-6'>
-                    <button className='p-4 bg-blue-500 text-white rounded-md' onClick={() => handleAddReader()}>Thêm</button>
-                    <button className='p-4 bg-red-500 text-white rounded-md' onClick={() => setShowModal(false)}>Hủy</button>
-                </div>
-            </div>
+            {option === 'reader' && (
+                <>
+                    <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50'></div>
+                    <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white z-50 p-12'>
+                        <h1 className='font-semibold text-[30px]'>Thêm độc giả</h1>
+                        <div className='w-[300px]'>
+                            <label className='font-semibold'>Tên độc giả</label>
+                            <input
+                                type="text"
+                                className='w-full p-2 border border-gray-300 rounded-md'
+                                value={readerName}
+                                onChange={(e) => setReaderName(e.target.value)}
+                            />
+                            <label className='font-semibold'>Ngày sinh</label><br />
+                            <input className='border p-1 rounded-md' type="date" name="" id=""
+                                value={birthday}
+                                onChange={(e) => setBirthday(e.target.value)}
+                            /><br />
+                            <label className='font-semibold' htmlFor="">Password</label>
+                            <input
+                                type="text"
+                                className='w-full p-2 border border-gray-300 rounded-md'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <label className='font-semibold'>Email</label>
+                            <input
+                                type="text"
+                                className='w-full p-2 border border-gray-300 rounded-md'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <label className='font-semibold'>Loại độc giả</label>
+                            <select name="" id="" className='px-3 py-1' onChange={(e) => setReaderTypeId(e.target.value)}>
+                                {readerType.length > 0 && readerType.map((item) => {
+                                    return (
+                                        <option value={item.reader_type_id}>{item.reader_type}</option>
+                                    )
+                                })}
+                            </select><br />
+                            <label className='font-semibold' htmlFor="">Địa chỉ</label>
+                            <input
+                                type="text"
+                                className='w-full p-2 border border-gray-300 rounded-md'
+                                value={address}
+                                onChange={(e) => setAddress(e.target.value)}
+                            />
+                        </div>
+                        <div className='flex justify-end gap-4 mt-6'>
+                            <button className='p-4 bg-blue-500 text-white rounded-md' onClick={() => handleAddReader()}>Thêm</button>
+                            <button className='p-4 bg-red-500 text-white rounded-md' onClick={() => setShowModal(false)}>Hủy</button>
+                        </div>
+                    </div>
+                </>
+            )}
+            {option === 'readerType' && (
+                <>
+                    <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50'></div>
+                    <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white z-50 p-12'>
+                        <h1 className='font-semibold text-[30px]'>Thêm loại độc giả</h1>
+                        <div className='w-[300px]'>
+                            <label className='font-semibold'>Tên loại độc giả</label>
+                            <input
+                                type="text"
+                                className='w-full p-2 border border-gray-300 rounded-md'
+                                value={readerName}
+                                onChange={(e) => setReaderName(e.target.value)}
+                            />
+
+                        </div>
+                        <div className='flex justify-end gap-4 mt-6'>
+                            <button className='p-4 bg-blue-500 text-white rounded-md' onClick={() => handleAddReader()}>Thêm</button>
+                            <button className='p-4 bg-red-500 text-white rounded-md' onClick={() => setShowModal(false)}>Hủy</button>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
--- src/pages/private/Reader.jsx
+++ src/pages/private/Reader.jsx
@@ -3,13 +3,14 @@ import React, { useEffect, useState } from 'react'
 import { Button } from '../../components/public'
 import { getAllReaders } from '../../apis/Reader'
 import { ReaderModal } from '../../components/private'
-import {formatTime} from '../../ultils/helpers'
+import { formatTime } from '../../ultils/helpers'
 import { payPenalty } from '../../apis/User'
 import { toast } from 'react-toastify'
 
 const Reader = () => {
   const [reader, setReader] = useState([{}])
   const [showModal, setShowModal] = useState(false)
+  const [option, setOption] = useState('reader')
   const getReader = async () => {
     const response = await getAllReaders()
     setReader(response.data)
@@ -20,31 +21,84 @@ const Reader = () => {
       getReader()
       toast.success('Trả nợ thành công')
     }
-    else{
+    else {
       toast.error('Trả nợ thất bại')
     }
   }
   useEffect(() => {
     getReader()
-  }, [])
+  }, [showModal])
   return (
     <div>
-      {showModal && <ReaderModal setShowModal={setShowModal} />}
-      <div className='w-[180px]'>
+      {showModal && <ReaderModal option={option} setShowModal={setShowModal} />}
+      <div className='flex items-center justify-center gap-4'>
+        <div
+          className={`w-[180px] text-center bg-gray-100 p-2 rounded-md cursor-pointer ${option === 'reader' && 'text-orange-500'}`}
+          onClick={() => { setOption('reader') }}
+        >Độc giả</div>
+        <div
+          className={`w-[180px] text-center bg-gray-100 p-2 rounded-md cursor-pointer ${option === 'readerType' && 'text-orange-500'}`}
+          onClick={() => { setOption('readerType') }}
+        >Loại độc giả</div>
+      </div>
+      {option === 'reader' && (
+        <>
+          <div className='w-[180px]'>
+            <Button
+              name='Thêm độc giả'
+              onClick={() => { setShowModal(true) }}
+            />
+          </div>
+          <table className='m-auto mt-6 w-full border-collapse shadow-lg '>
+            <thead className='bg-gray-100  border-gray-300'>
+              <tr className='' >
+                <th className='px-6 py-4 text-[16px] font-medium'>Mã độc giả</th>
+                <th className='px-6 py-4 text-[16px] font-medium'>Tên độc giả</th>
+                <th className='px-6 py-4 text-[16px] font-medium'>Loại độc giả</th>
+                <th className='px-6 py-4 text-[16px] font-medium'>Ngày lập thẻ</th>
+                <th className='px-6 py-4 text-[16px] font-medium'>Ngày hết hạn</th>
+                <th className='px-6 py-4 text-[16px] font-medium'>Tổng nợ</th>
+                <th className='px-6 py-4 text-[16px] font-medium'>Thao tác</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reader.length > 0 && reader.map((item) => {
+                return (
+                  <tr key={item.reader_id} className=''>
+                    <td className='px-6 py-4 text-center border-b'>{item.user_id}</td>
+                    <td className='px-6 py-4 text-center border-b'>{item.user_name}</td>
+                    <td className='px-6 py-4 text-center border-b'>{item.reader_type === null ? 'không có' : item.reader_type}</td>
+                    <td className='px-6 py-4 text-center border-b'>{formatTime(new Date(item.created_at))}</td>
+                    <td className='px-6 py-4 text-center border-b'>{formatTime(new Date(item.expiry_date))}</td>
+                    <td className='px-6 py-4 text-center border-b'>{item.penalty_owed}</td>
+                    <td className='flex justify-center items-center border-b '>
+                      <button className='mr-2 w-[80px] p-1 bg-blue-500 text-white rounded-md hover:bg-blue-700'>Sửa</button>
+                      <button className='p-1 w-[80px] bg-red-500 text-white rounded-md hover:bg-red-700'>Xóa</button>
+                      {item.penalty_owed > 0 && <button
+                        className='p-1 ml-1 w-[80px] bg-cyan-500 text-white rounded-md hover:bg-cyan-700'
+                        onClick={() => handlePayPenalty({ user_id: item.user_id, amount: item.penalty_owed })}
+                      >Trả nợ</button>}
+                    </td>
+                  </tr>
+                )
+              })
+              }
+            </tbody>
+          </table>
+        </>
+      )}
+      {option === 'readerType' && (
+        <>
+          <div className='w-[180px]'>
         <Button
-          name='Thêm độc giả'
+          name='Thêm loại độc giả'
           onClick={() => { setShowModal(true) }}
         />
       </div>
       <table className='m-auto mt-6 w-full border-collapse shadow-lg '>
         <thead className='bg-gray-100  border-gray-300'>
           <tr className='' >
-            <th className='px-6 py-4 text-[16px] font-medium'>Mã độc giả</th>
-            <th className='px-6 py-4 text-[16px] font-medium'>Tên độc giả</th>
-            <th className='px-6 py-4 text-[16px] font-medium'>Loại độc giả</th>
-            <th className='px-6 py-4 text-[16px] font-medium'>Ngày lập thẻ</th>
-            <th className='px-6 py-4 text-[16px] font-medium'>Ngày hết hạn</th>
-            <th className='px-6 py-4 text-[16px] font-medium'>Tổng nợ</th>
+            <th className='px-6 py-4 text-[16px] font-medium'>Tên loại độc giả</th>
             <th className='px-6 py-4 text-[16px] font-medium'>Thao tác</th>
           </tr>
         </thead>
@@ -52,26 +106,15 @@ const Reader = () => {
           {reader.length > 0 && reader.map((item) => {
             return (
               <tr key={item.reader_id} className=''>
-                <td className='px-6 py-4 text-center border-b'>{item.user_id}</td>
-                <td className='px-6 py-4 text-center border-b'>{item.user_name}</td>
-                <td className='px-6 py-4 text-center border-b'>{item.reader_type===null?'không có':item.reader_type}</td>
-                <td className='px-6 py-4 text-center border-b'>{formatTime(new Date(item.created_at))}</td>
-                <td className='px-6 py-4 text-center border-b'>{formatTime(new Date(item.expiry_date))}</td>
-                <td className='px-6 py-4 text-center border-b'>{item.penalty_owed}</td>
-                <td className='flex justify-center items-center border-b '>
-                  <button className='mr-2 w-[80px] p-1 bg-blue-500 text-white rounded-md hover:bg-blue-700'>Sửa</button>
-                  <button className='p-1 w-[80px] bg-red-500 text-white rounded-md hover:bg-red-700'>Xóa</button>
-                  {item.penalty_owed >0 && <button 
-                                            className='p-1 ml-1 w-[80px] bg-cyan-500 text-white rounded-md hover:bg-cyan-700'
-                                            onClick={()=>handlePayPenalty({user_id:item.user_id,amount:item.penalty_owed})}
-                                            >Trả nợ</button>}
-                </td>
+                
               </tr>
             )
           })
           }
         </tbody>
       </table>
+        </>
+      )}
     </div>
   )
 }
