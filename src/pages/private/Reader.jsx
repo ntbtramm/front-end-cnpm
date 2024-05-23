3dd8bc631a82d273d66d33d880160dfd1b1e111b
Toan <nvantoan1203@gmail.com>
2024-05-24 01:46:50 +0700

    add phieu_thu

--- src/pages/private/Reader.jsx
+++ src/pages/private/Reader.jsx
@@ -4,43 +4,55 @@ import { Button } from '../../components/public'
 import { getAllReaderType, getAllReaders, getOneReaderType } from '../../apis/Reader'
 import { ReaderModal } from '../../components/private'
 import { formatTime } from '../../ultils/helpers'
-import { payPenalty, Delete_user } from '../../apis/User'
+import { payPenalty, Delete_user, getOneUser } from '../../apis/User'
 import { toast } from 'react-toastify'
 
 const Reader = () => {
-  const [reader, setReader] = useState([{}])
-  const [readerType, setReaderType] = useState([{}])
-  const [showModal, setShowModal] = useState(false)
-  const [option, setOption] = useState('reader')
+  const [reader, setReader] = useState([{}]);
+  const [readerType, setReaderType] = useState([{}]);
+  const [showModal, setShowModal] = useState(false);
+  const [user_pt, set_user_pt] = useState([]);
+  const [user_id, set_user_id] = useState(null);
+  const [option, setOption] = useState('reader');
   const getReader = async () => {
-    const response = await getAllReaders()
-    setReader(response.data)
+    const response = await getAllReaders();
+    setReader(response.data);
+
+  }
+
+  const handlept = async(id) => {
+    const response = await getOneUser(id);
+    set_user_pt(response.data);
+    set_user_id(id);
+  }
+  const handleCancel = () =>{
+    set_user_id(null);
   }
   const getReaderType = async () => {
-    const response = await getAllReaderType()
-    setReaderType(response.data)
+    const response = await getAllReaderType();
+    setReaderType(response.data);
   }
   const handlePayPenalty = async (data) => {
-    const response = await payPenalty(data)
+    const response = await payPenalty(data);
     if (response.status === 200) {
-      getReader()
-      toast.success('Trả nợ thành công')
+      getReader();
+      toast.success('Trả nợ thành công');
     }
     else {
-      toast.error('Trả nợ thất bại')
+      toast.error('Trả nợ thất bại');
     }
   }
 
   const handleDelete_user = async (user_id) => {
     if (confirm('Are you sure?')) {
 
-      const response = await Delete_user(user_id)
+      const response = await Delete_user(user_id);
       if (response.status === 200) {
-        getReader()
-        toast.success('Xóa thành công')
+        getReader();
+        toast.success('Xóa thành công');
       }
       else {
-        toast.error('Xóa thất bại')
+        toast.error('Xóa thất bại');
       }
     }
 
@@ -48,8 +60,8 @@ const Reader = () => {
 
 
   useEffect(() => {
-    getReader()
-    getReaderType()
+    getReader();
+    getReaderType();
   }, [showModal])
   console.log(reader)
   return (
@@ -75,7 +87,7 @@ const Reader = () => {
           </div>
           <table className='m-auto mt-6 w-full border-collapse shadow-lg '>
             <thead className='bg-gray-100  border-gray-300'>
-              <tr className='' >
+              <tr className=''>
                 <th className='px-6 py-4 text-[16px] font-medium'>Mã độc giả</th>
                 <th className='px-6 py-4 text-[16px] font-medium'>Tên độc giả</th>
                 <th className='px-6 py-4 text-[16px] font-medium'>Loại độc giả</th>
@@ -88,27 +100,65 @@ const Reader = () => {
             <tbody>
               {reader.length > 0 && reader.map((item) => {
                 return (
-                  <tr key={item.reader_id} className=''>
-                    <td className='px-6 py-4 text-center border-b'>{item.user_id}</td>
-                    <td className='px-6 py-4 text-center border-b'>{item.user_name}</td>
-                    <td className='px-6 py-4 text-center border-b'>{item.reader_type === null ? 'không có' : item.reader_type}</td>
-                    <td className='px-6 py-4 text-center border-b'>{formatTime(new Date(item.created_at))}</td>
-                    <td className='px-6 py-4 text-center border-b'>{formatTime(new Date(item.expiry_date))}</td>
-                    <td className='px-6 py-4 text-center border-b'>{item.penalty_owed}</td>
-                    <td className='flex justify-center items-center border-b '>
-                      <button className='mr-2 w-[80px] p-1 bg-blue-500 text-white rounded-md hover:bg-blue-700'>Sửa</button>
-                      <button
-                        className='p-1 w-[80px] bg-red-500 text-white rounded-md hover:bg-red-700' onClick={() => handleDelete_user(item.user_id)}>Xóa</button>
+                  <React.Fragment key={item.reader_id}>
+                    <tr className=''>
+                      <td className='px-6 py-4 text-center border-b'>{item.user_id}</td>
+                      <td className='px-6 py-4 text-center border-b'>{item.user_name}</td>
+                      <td className='px-6 py-4 text-center border-b'>{item.reader_type === null ? 'không có' : item.reader_type}</td>
+                      <td className='px-6 py-4 text-center border-b'>{formatTime(new Date(item.created_at))}</td>
+                      <td className='px-6 py-4 text-center border-b'>{formatTime(new Date(item.expiry_date))}</td>
+                      <td className='px-6 py-4 text-center border-b'>{item.penalty_owed}</td>
+                      <td className='flex justify-center items-center border-b '>
+                        <button className='mr-2 w-[80px] p-1 bg-blue-500 text-white rounded-md hover:bg-blue-700'>Sửa</button>
+                        <button
+                          className='p-1 w-[80px] bg-red-500 text-white rounded-md hover:bg-red-700' onClick={() => handleDelete_user(item.user_id)}>Xóa</button>
+  
+                        {item.penalty_owed > 0 && <button
+                          className='p-1 ml-1 w-[80px] bg-cyan-500 text-white rounded-md hover:bg-cyan-700'
+                          onClick={() => handlePayPenalty({ user_id: item.user_id, amount: prompt("nhập số tiền") })}
+                        >Trả nợ</button>}
+                        {item.penalty_owed > 0 && <button
+                          className='p-1 ml-1 w-[80px] bg-cyan-500 text-white rounded-md hover:bg-cyan-700'
+                          onClick={() => handlept(item.user_id)}
+                        >Phiếu thu tiền</button>}
+                      </td>
+                    </tr>
+                    {item.user_id === user_id && (
+                      <tr>
+                        <td colSpan="3" className="px-6 py-4 text-center border-b">
+                          <div className="flex flex-col items-center">
+                            <div className="mb-4 text-lg font-medium">Thông tin phiếu thu tiền</div>
+                            <div className="mb-2 text-base font-medium">
+                              <label className="mr-4">Mã độc giả:</label>
+                              <span>{item.user_id}</span>
+                            </div>
+                            {user_pt.fine_collections && user_pt.fine_collections.map((fine, index) => (
+                              <div key={index} className="mb-2 text-base font-medium">
+                                <label className="mr-4">Ngày lập phiếu:</label>
+                                <span>{formatTime(new Date(fine.created_at))}</span>
+                                <label className="ml-4 mr-4">Tiền thu:</label>
+                                <span>{fine.amount}</span>
+                              </div>
+                            ))}
+                            <div className="mb-2 text-base font-medium">
+                              <label className="mr-4">Tổng nợ:</label>
+                              <span>{item.penalty_owed}</span>
+                            </div>
+                            <button
+                              className="w-[100px] py-2 bg-gray-500 text-white rounded-md hover:bg-gray-700 mt-4"
+                              onClick={handleCancel}
+                            >
+                              Hủy
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    )}
 
-                      {item.penalty_owed > 0 && <button
-                        className='p-1 ml-1 w-[80px] bg-cyan-500 text-white rounded-md hover:bg-cyan-700'
-                        onClick={() => handlePayPenalty({ user_id: item.user_id, amount: prompt("nhập số tiền") })}
-                      >Trả nợ</button>}
-                    </td>
-                  </tr>
+
+                  </React.Fragment>
                 )
-              })
-              }
+              })}
             </tbody>
           </table>
         </>
@@ -123,7 +173,7 @@ const Reader = () => {
           </div>
           <table className='m-auto mt-6 w-full border-collapse shadow-lg '>
             <thead className='bg-gray-100  border-gray-300'>
-              <tr className='' >
+              <tr className=''>
                 <th className='px-6 py-4 text-[16px] font-medium'>Tên loại độc giả</th>
                 <th className='px-6 py-4 text-[16px] font-medium'>Thao tác</th>
               </tr>
@@ -139,14 +189,13 @@ const Reader = () => {
                     </td>
                   </tr>
                 )
-              })
-              }
+              })}
             </tbody>
           </table>
         </>
       )}
     </div>
-  )
-}
+  )}
+  
 
 export default Reader
